2b8cfc041d6618e0ec5a71cdb01e7c3d27fcb519
eshan786 <eshan786behal@gmail.com>
2019-10-08 10:07:09 +0530

    written logic steps.

---
+++ jquery.js
@@ -0,0 +1,20 @@
+//click on stasrt/reset button
+    //are we playing?
+      //yes
+        //reload page
+      //no
+        //show trials left.
+        //change button text to reset game.
+        //1.create a random fruit.
+        //define a random step.
+        //2.move the fruit down by one step in loop every 30 sec.
+           //2.is fruit too low?.
+              //no -> repeat 2..
+              //yes -> any trials left.
+              //yes -> repeat no 1.
+              //no -> show game over; buttontext : start game.
+            
+
+//slice a fruit
+   //play sound.
+   //explode fruit.
